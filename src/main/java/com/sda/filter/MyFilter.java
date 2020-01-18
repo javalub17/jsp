314cb9a91d3ae75ae6e71a41f67a5eff7096a51c
lkre <lukasz.res@gmail.com>
2020-01-18 14:26:38 +0100

    zadanie 25

--- src/main/java/com/sda/filter/MyFilter.java
+++ src/main/java/com/sda/filter/MyFilter.java
@@ -2,9 +2,12 @@ package com.sda.filter;
 
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebFilter(filterName = "MyFilter", urlPatterns = {"/hello"})
+//@WebFilter(filterName = "MyFilter", servletNames = {"HelloWorldServlet"})
 public class MyFilter implements Filter {
     @Override
     public void doFilter(
@@ -15,9 +18,9 @@ public class MyFilter implements Filter {
         writer.write("<html><body><div style='text-align: center;'>");
         String login = servletRequest.getParameter("login");
 
-        if(login != null && login.equals("admin")){
+        if (login != null && login.equals("admin")) {
             filterChain.doFilter(servletRequest, servletResponse);
-        } else{
+        } else {
             writer.print("<p style='color: red; font-size: larger;'>" +
                     "User name is invalid" +
                     "</p>");
